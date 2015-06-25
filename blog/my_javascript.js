9102cbedb8617b3d8a661621690e575a9b566a66
Sam Rushing <srushing@sanguinebio.com>
2015-06-24 19:31:51 -0700

    eye opens and closes now

--- blog/my_javascript.js
+++ blog/my_javascript.js
@@ -3,15 +3,19 @@ function toggleBackground() {
   var graybg = document.getElementById("boxone");
   var mytext = document.getElementById("textcontainertwo");
   var textbox = document.getElementById("listenbox");
+  var eyeicon = document.getElementById("eyeicon")
 
   if (graybg.classList.contains("invisible")) {
+      eyeicon.src = "../images/eyeclose.png";
       graybg.className = "slidein";
       mytext.className = "fadein two";
       textbox.className = "invisible";
   } else if (graybg.classList.contains("slidein")) {
+      eyeicon.src = "../images/eyeicon.png"
       graybg.className = "slideout";
       mytext.className = "fadeout";
   } else if (graybg.classList.contains("slideout")) {
+      eyeicon.src = "../images/eyeclose.png"
       graybg.className = "slidein";
       mytext.className = "fadein two";
       textbox.className = "invisible";
